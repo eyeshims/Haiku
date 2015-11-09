bf311ba35046c0fe6b1d217b48eed8cb03a5c60e
eyeshims <eyeshims@gmail.com>
2015-11-09 14:26:29 +1300

    Add button to main activity

--- app/src/main/java/com/haiku/MainActivity.java
+++ app/src/main/java/com/haiku/MainActivity.java
@@ -3,6 +3,7 @@ package com.haiku;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onLoveButtonClicked(View view) {
-
+        TextView haiku = (TextView) findViewById(R.id.haikuTextView);
+        haiku.setVisibility(View.VISIBLE);
     }
 }
 
